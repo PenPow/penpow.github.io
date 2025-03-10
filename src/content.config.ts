ec5da916dfcd9d6c29eca8e3d39b58d2fb260ced
PenPow <josh@penpow.dev>
2025-03-10 20:15:09 +0000

    🔄 created local 'src/' from remote 'src/'

---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { defineCollection, z } from "astro:content";
+import { rssSchema } from '@astrojs/rss';
+import { glob } from 'astro/loaders';
+
+const postsCollection = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/posts" }),
+	schema: rssSchema.extend({ 
+		hidden: z.boolean().optional().default(false),
+		description: z.string(),
+		title: z.string(),
+		pubDate: z.date() 
+	}),
+});
+
+const papersCollection = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/papers" }),
+	schema: rssSchema.extend({ 
+		hidden: z.boolean().optional().default(false),
+		description: z.string(),
+		title: z.string(),
+		pubDate: z.date(),
+		pdf: z.string().endsWith(".pdf"),
+	}),
+});
+
+
+export const collections = {
+	'posts': postsCollection,
+	'papers': papersCollection
+}
